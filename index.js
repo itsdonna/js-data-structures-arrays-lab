0dc7632239d66a3b94cd6009e1bafd34f0f3ec5c
Donna Torres <todonnatorres@icloud.com>
2021-01-30 01:02:33 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver (name) { drivers.push(name); }
+
+function destructivelyPrependDriver(name) { drivers.unshift(name); }
+
+function destructivelyRemoveLastDriver() { drivers.pop(); }
+
+function destructivelyRemoveFirstDriver() { drivers.shift(); }
+
+function appendDriver(name) { const copyOfDriver = [...drivers, name];
+return copyOfDriver; } // spread operator functions allows adding new elements but keeping the original 
+
+function prependDriver(name) { const copyOfDrivers = [name, ...drivers ];
+return copyOfDrivers}
+
+function removeLastDriver() { const lastDrivers = drivers.slice(0, 2);
+return lastDrivers }
+
+function removeFirstDriver() {const firstDrivers = drivers.slice(-2);
+return firstDrivers; } 
